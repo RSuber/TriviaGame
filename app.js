a7b5b2a6c89aafda808033d8ff8d403048ffd570
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-07-07 09:00:01 -0400

    changes

--- app.js
+++ app.js
@@ -1,62 +1,85 @@
-var app = angular.module('myApp', []);
-
-app.controller('TriviaControl',function($scope,$http){
-  $scope.correct = [{
-    // answer:''
-  }];
-
-  $scope.incorrect = [{
-    // answer:''
-  }];
-
-  $scope.doubles = [{
-    // answer:''
-  }]
-
-  $scope.toanswer =[{
-    // answer:''
-  }];
-
-  $scope.trivia =[{
-    // question:''
-  }];
-  $scope.lastQuestion=[{
-
-  }]
-  $scope.userAnswer =''
-  $scope.score = 0
-  $scope.value = 0
+var app = angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            redirectTo: '/intro',
+        })
+        .when('/intro', {
+            templateUrl: 'Templates/Intro.html',
+        })
+        .when('/maingame', {
+            controller: 'TriviaControl',
+            templateUrl: 'Templates/MainGame.html',
+        })
+        .when('/newgame', {
+            templateUrl: 'Templates/EndGame.html',
+        })
+}]);
+app.controller('TriviaControl',['GameFactory','$scope',function(GameFactory,$scope){
 $scope.showQuestion = function(){
-  $http({
-    method:'GET',
-    url: 'http://jservice.io/api/random',
-  }).then(function(response){
+  GameFactory.trivia().then(function(){
     let data = response.data[0]
     $scope.toanswer.answer = data.answer;
     $scope.trivia.question= data.question;
     console.log(data)
-
   });
 }
+
 //I was trying to get rid of doubles but couldnt figure it out.
 $scope.compareAnswer = function(){
   if($scope.userAnswer === $scope.toanswer.answer){
-    $http({
-      method:'GET',
-      url: 'http://jservice.io/api/random',
-    }).then(function(response){
-      let data= response.data[0]
-       $scope.value = data.value;
-      $scope.score = $scope.score + data.value;
-      $scope.correct.push({
-        answer: $scope.toanswer.answer,
-        value : $scope.value
-      });
-    })
+    GameFactory.trivia()
     }
     else if($scope.userAnswer !== $scope.toanswer.answer){
         $scope.incorrect.push({answer:$scope.userAnswer});
     }
     $scope.showQuestion()
   };
-});
+$scope.showCategory = function(){
+  $scope.categories = [{}]
+  GameFactory.category($scope.categories)
+}
+$scope.showCategory()
+}]);
+app.factory('GameFactory', ['$http',function($http){
+  let correct =[]
+  let incorrect = []
+  let toanswer = []
+  let trivia = []
+  let value = 0
+  let score = 0
+  let categories=[]
+  var gettrivia = function(){
+    console.log('trivia')
+    return $http({
+    method:'GET',
+    url: 'http://jservice.io/api/random',
+  }).then(function(response){
+      let data= response.data[0]
+      value= data.value;
+      score = score + data.value;
+      correct.push({
+      answer: data.answer,
+      value : data.value
+    });
+  })
+};
+  var getcategory = function(som){
+      var disposable = []
+    return $http({
+      method:'GET',
+      url:'http://jservice.io/api/categories?count=6',
+    }).then(function(response){
+    response.data.forEach(function(el){
+        som.push({title:el.title})
+        disposable.push(el)
+      });
+      angular.copy(disposable, categories);
+       console.log(categories)
+    })
+  };
+  return {
+    trivia : gettrivia,
+    category: getcategory,
+  }
+}]);
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,34 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var app = angular.module('myApp', []);
-
+var app = angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/', {
+    redirectTo: '/Templates/Intro.html'
+  }).when('/intro', {
+    controller: 'TriviaControl',
+    templateUrl: 'Templates/Intro.html'
+  }).when('/maingame', {
+    controller: 'TriviaControl',
+    templateUrl: 'Templates/MainGame.html'
+  }).when('/newgame', {
+    controller: 'TriviaControl',
+    templateUrl: 'Templates/EndGame.html'
+  });
+}]);
 app.controller('TriviaControl', function ($scope, $http) {
-  $scope.answered = [{ answer: 'POOPY' }];
+  $scope.correct = [{}];
+  $scope.incorrect = [{}];
 
-  $scope.toanswer = [{ answer: 'asdf' }];
+  $scope.doubles = [{}];
 
-  $scope.trivia = [{
-    question: 'poop'
-  }];
+  $scope.toanswer = [{}];
 
+  $scope.trivia = [{}];
+  $scope.lastQuestion = [{}];
   $scope.userAnswer = '';
-
+  $scope.score = 0;
+  $scope.value = 0;
   $scope.showQuestion = function () {
     $http({
       method: 'GET',
@@ -22,12 +37,28 @@ app.controller('TriviaControl', function ($scope, $http) {
       var data = response.data[0];
       $scope.toanswer.answer = data.answer;
       $scope.trivia.question = data.question;
-      $scope.answered.push({ answer: $scope.toanswer.answer });
-      console.log($scope.answered);
+      console.log(data);
     });
   };
+  //I was trying to get rid of doubles but couldnt figure it out.
   $scope.compareAnswer = function () {
-    console.log();
+    if ($scope.userAnswer === $scope.toanswer.answer) {
+      $http({
+        method: 'GET',
+        url: 'http://jservice.io/api/random'
+      }).then(function (response) {
+        var data = response.data[0];
+        $scope.value = data.value;
+        $scope.score = $scope.score + data.value;
+        $scope.correct.push({
+          answer: $scope.toanswer.answer,
+          value: $scope.value
+        });
+      });
+    } else if ($scope.userAnswer !== $scope.toanswer.answer) {
+      $scope.incorrect.push({ answer: $scope.userAnswer });
+    }
+    $scope.showQuestion();
   };
 });
 },{}]},{},[1])
